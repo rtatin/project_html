e70112c9f6af5521a325556ceeb1150245ed003b
theophile <theophilevonck@gmail.com>
2019-10-05 18:19:46 +0200

    account_page premier jet

--- account_page.php
+++ account_page.php
@@ -1,1 +1,22 @@
-test
+<!DOCTYPE html>
+<?php include 'header.php';
+
+?>
+<html>
+    <head>
+        <title>creation de compte</title>
+        <link rel="stylesheet" href="account_page.css" />
+    </head>
+<body>
+<div id="login-box"></div>
+<div class="box"></div>
+
+<h1>crée un compte</h1>
+
+<input type="text" name="username" placeholder="Nom d utilisateur"/>
+<input type="text" name="email" placeholder="Email"/>
+<input type="password" name="password" placeholder="mots de passe"/>
+<input type="password" name="password2" placeholder="mots de passe"/>
+<input type="submit" name="signup-button" placeholder="envoyer"/>
+</body>
+</html>
