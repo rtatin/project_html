62dbcded5922bcc06988d86ed31cd10144593609
theophile <theophilevonck@gmail.com>
2019-10-07 16:33:20 +0200

    changement account et head

--- header.php
+++ header.php
@@ -3,33 +3,54 @@
 <head>
     <title>titre</title>
     <link rel="stylesheet" href="head.css" />
+    
 </head>
 
 <body>
     <header>
         <nav>
-            <div class="menu">
-            <a href="main.php">OUI</a>
-            </div>
-
+            <span>titre</span>
             <div class="menuCategory">
-            <div class="tab">
   <button class="tablinks" onclick="window.location.href='search.php'">search</button>
   <button class="tablinks" onclick="openCity(event, 'Recipe')">product page</button>
   <button class="tablinks" onclick="window.location.href='account_page.php'">crée un compte</button>
   <button class="tablinks" onclick="openCity(event, 'Recipe')">cart page</button>
-</div>
-		    	
-
-<p id="identification" onclick="pannaux()">s'identifier</p>
-
-
-
-<input type="text" name="username" placeholder="Nom d utilisateur"/>
-<input type="password" name="password" placeholder="mots de passe"/>
-            </div>
-            
         </nav>
+
+        <div class login_box>
+        <form class="identify">
+        <table>
+        <tr>
+        <td>Username</td>
+        <td>Password</td>
+        </tr>
+        <tr>
+        <td><div class="textbox">
+            <input type="text" placeholder="Username" name="" value="">
+            </div>
+        </td>
+        <td>
+            <div class="textbox">
+            <input type="password" placeholder="Password" name="" value="">
+            </div>
+        </td>
+        <td>
+        <input type="submithead" name="" value="envoyer">
+        </td>
+        </tr>
+        <tr>
+        <td>
+        <div><a href="account_page.php">crée un compte</a></div>
+        </td>  
+        </tr>
+
+
+
+</table>
+</form>
+
+
+</div>
     </header>
 
 </body>
