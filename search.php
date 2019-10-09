445fcc4b75e7a0f4fedcfbf4dc5b2821bce6ec0b
Martin <martin.barres@isen.yncrea.fr>
2019-10-09 17:19:01 +0200

    page recherche (sans fonction)

---
+++ search.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>search</title>
+    <link rel="stylesheet" href="head.css" />
+    
+</head>
+
+<body>
+    <header>
+        <nav>
+            <span>Website</span>
+            <div class="menuCategory">
+  <button class="tablinks" onclick="window.location.href='search.php'">Search</button>
+  <div class="dropdown">
+  <button class="tablinks" onclick="window.location.href='product_page.php'">product page</button>
+  <div class="dropdown-content">
+    <a id="linkhead" href="#">Link 1</a>
+    <a id="linkhead" href="#">Link 2</a>
+    <a id="linkhead" href="#">Link 3</a>
+  </div>
+  </div>
+  <button class="tablinks" onclick="window.location.href='account_page.php'">Create account</button>
+  <button class="tablinks" onclick="window.location.href='cart_page.php'">cart page</button>
+   </nav>
+        <div class login_box>
+        <form class="identify">
+        <table>
+            
+        <tr>
+        <td>Username</td>
+        <td>Password</td>
+        </tr>
+
+        <tr>
+        <td>
+            <input id="textbox" type="text" placeholder="Username" name="" value="">
+        </td>
+        <td>
+            <input id="textbox" type="password" placeholder="Password" name="" value="">
+        </td>
+        <td>
+            <input id="submithead" type="submit"  name="" value="send">
+        </td>
+        </tr>
+
+        <tr>
+        <td>
+        <div class="withSeveralLinks"><a href="account_page.php">Create account</a></div>
+        </td>  
+        </tr>
+</table>
+</form>
+</div>
+    </header>
+</body>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href="footer.css" />
+</head>
+<body>
+  <footer>
+        <div class="withSeveralLinks">
+        <a href="http://www.cdiscount.fr">Our company</a><br/>
+        <a href="product_page.php">Our products</a><br/>
+        <a href="http://www.cdiscount.fr">Contact us</a><br/>
+        </div>
+        <button class="buttonFooter" onclick="window.location.href='#top'">Haut de page</button>
+
+        <p>Nom du site - Tous droits réservés</p>
+        
+      </footer>
+</body>
+</html></html>
+
+
+<html>
+<div style="width:25px;height:700px;overflow:auto; margin:auto;"> 
+    <head>
+        <title>Recherche</title>
+        <link rel="stylesheet" href="account_page.css" />
+    </head>
+<body>
+<form class="formulaire">
+<h1>Vous cherchez un produit?</h1>
+<p>Entrez votre recherche ci-dessous:</p>
+<input id="useninfo" type="text" name="search" placeholder="Produit à trouver"/>
+</form>
+</body>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href="footer.css" />
+</head>
+<body>
+  <footer>
+        <div class="withSeveralLinks">
+        <a href="http://www.cdiscount.fr">Our company</a><br/>
+        <a href="product_page.php">Our products</a><br/>
+        <a href="http://www.cdiscount.fr">Contact us</a><br/>
+        </div>
+        <button class="buttonFooter" onclick="window.location.href='#top'">Haut de page</button>
+
+        <p>Nom du site - Tous droits réservés</p>
+        
+      </footer>
+</body>
+</html></div>
+</html>
