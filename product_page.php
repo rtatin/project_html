fce658e9093ddacedbacbd523d59efc6dd8e11f6
theophile <theophilevonck@gmail.com>
2019-10-25 17:35:18 +0200

    test

--- product_page.php
+++ product_page.php
@@ -8,7 +8,11 @@
 </head>
 <body id="body1">
 <center><caption><font size="25" face="Calibri"><b>Nos Produits</b></font></caption></center>
-<?php foreach (getAllProducts() as $products) {?>
+<br>
+<?php foreach (getAllRange() as $range) {?>
+        <h1 id="cat_<?php echo $range["id"] ?>"> <?php echo $range["name"];?> </h1>
+
+<?php foreach (getAllProductByRange($range["id"]) as $products) {?>
     <center>
 <table id="table1" border>
     <thead><tr>
@@ -46,8 +50,7 @@
             <dd>
             <a style="border-style: none;" cols = 10 rows = 2>
             prix:
-            <?php echo $products["unit_price"];?>
-            €
+            <?php echo $products["unit_price"];?>€
             </a>
             </dd>
             <br>
@@ -56,11 +59,12 @@
             Quantité : <input type="number" name="quantite" size="3"><br>
             <input type="submit" value="OK">
             </form>
-        </td>
+            </td>
     </tr></tbody>
 </table>
 </div>
 </center>
+<?php }?>
 <br>
 <br>
 <br>
